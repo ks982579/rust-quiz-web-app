9c28c84238c58b947c18307abc7a22847a397ee5
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2024-06-01 16:53:39 +0100

    Adding logic to send requests for create user front-end

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,8 @@
 use leptos::*;
 use leptos_router::{Form, Route, Router, Routes, A};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 fn main() {
     mount_to_body(App)
@@ -66,26 +69,72 @@ fn LogIn() -> impl IntoView {
     }
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct NewUserFormData {
+    name: String,
+    username: String,
+    password: String,
+}
+
 #[component]
 fn CreateNewUser() -> impl IntoView {
-    let (name, set_name): (ReadSignal<String>, WriteSignal<String>) =
-        create_signal("Uncontrolled".to_string());
-    let input_element: NodeRef<html::Input> = create_node_ref();
+    // let (name, set_name): (ReadSignal<String>, WriteSignal<String>) =
+    //     create_signal("Uncontrolled".to_string());
+
+    let name_input_elm: NodeRef<html::Input> = create_node_ref();
+    let username_input_elm: NodeRef<html::Input> = create_node_ref();
+    let password_input_elm: NodeRef<html::Input> = create_node_ref();
     // let on_submit: dyn FnOnce = move || {};
+
+    // Submit function for sending data to backend
     let on_submit = move |ev: ev::SubmitEvent| {
         // stop page reloading
         ev.prevent_default();
         // extract value from input
-        let value = input_element
-            .get()
+        let name_value: String = name_input_elm
+            .get() // Option<HtmlElement<T>>
             .expect("<input> should be mounted")
             // `leptos::HtmlElement<html::Input>` implements `Deref`
+            .value(); // -> String
+        let username_value: String = username_input_elm
+            .get()
+            .expect("<input> should be mounted")
             .value();
-        set_name.set(value);
+        let password_value: String = password_input_elm
+            .get()
+            .expect("<input> should be mounted")
+            .value();
+
+        // Package Data into JSON String
+        let pck: String = serde_json::json! ({
+            "name": name_value,
+            "username": username_value,
+            "password": password_value,
+        })
+        .to_string();
+        let request: Request = Request::new_with_str_and_init(
+            "http://localhost:8080/what",
+            &RequestInit::new()
+                .method("POST")
+                .mode(RequestMode::Cors)
+                .body(Some(&pck.into())),
+        )
+        .unwrap();
+
+        request.headers().set("Accept", "application/json");
+
+        spawn_local(async move {
+            let window = web_sys::window().unwrap();
+            let this: JsFuture = JsFuture::from(window.fetch_with_request(&request));
+            let that: Response = this.await.unwrap().into();
+        });
+        // set_name.set(value);
     };
     view! {
         <form on:submit=on_submit>
-            <input type="text" id="username" value=name node_ref=input_element />
+            <input type="text" id="name" node_ref=name_input_elm />
+            <input type="text" id="username" node_ref=username_input_elm />
+            <input type="text" id="password" node_ref=password_input_elm />
             <input type="submit" value="Join!" />
         </form>
     }
