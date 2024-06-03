31e718291a68e16c6252e5765c12071a515c5a3c
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2024-06-03 19:48:34 +0100

    Implementing checks and errror handling for create user endpoint

--- backend/src/routes/create_user.rs
+++ backend/src/routes/create_user.rs
@@ -1,12 +1,62 @@
 //! backend/src/routes/create_user.rs
 //! Endpoint used for user creation given credentials.
+use crate::error_chain_helper;
 use crate::surrealdb_repo::Database;
 use actix_web::http::header::ContentType;
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::http::StatusCode;
+use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
+use anyhow::Context;
 use models::GeneralUser;
 use serde::Deserialize;
 use uuid::Uuid;
 
+// Errors
+#[derive(thiserror::Error)]
+pub enum CreateUserError {
+    // #[error("{0}")]
+    // PasswordsDontMatch(String),
+    // #[error("{0}")]
+    // MissingName(String),
+    // #[error("{0}")]
+    // MissingUsername(String),
+    // #[error("{0}")]
+    // MissingPassword(String),
+    #[error("{0}")]
+    ValidationError(String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl std::fmt::Debug for CreateUserError {
+    /// Custom implementation to display root cause of errors
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_helper(self, f)
+    }
+}
+
+impl ResponseError for CreateUserError {
+    // not required method actually...
+    // fn status_code(&self) -> StatusCode {
+    //     match *self {
+    //         CreateUserError::ValidationError(_) => StatusCode::BAD_REQUEST,
+    //         CreateUserError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    //     }
+    // }
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        match self {
+            CreateUserError::UnexpectedError(_) => {
+                HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR)
+                    .insert_header(ContentType::json())
+                    .json(serde_json::json!({"msg": "Unknown Error"}))
+            }
+            CreateUserError::ValidationError(msg) => HttpResponse::build(StatusCode::BAD_REQUEST)
+                .insert_header(ContentType::json())
+                .json(serde_json::json!({ "msg": msg })),
+        }
+    }
+}
+
+// Structs for JSON
 #[derive(Debug, Clone, Deserialize)]
 pub struct CreateUserPayload {
     name: String,
@@ -31,17 +81,20 @@ pub async fn create_user(
     req: HttpRequest,
     db: web::Data<Database>,
     user_info_pt: web::Json<CreateUserPayload>,
-) -> HttpResponse {
+) -> Result<HttpResponse, CreateUserError> {
     let user_data = user_info_pt.into_inner();
 
     // Is username unique?
     let users = db
         .count_users(&user_data.username)
         .await
-        .expect("issue connecting");
+        .context("Issue performing count")?;
 
+    // Turn this into function to unwrap with ?
     if users > 0 {
-        return HttpResponse::BadRequest().finish();
+        return Err(CreateUserError::ValidationError(
+            "Username already exists".to_string(),
+        ));
     }
 
     let new_user_opt: Option<GeneralUser> = db.add_general_user(user_data.into()).await;
