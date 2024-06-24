8eea8bdd6e47eab26baa4adc1e08018e8c31f29a
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2024-06-24 07:32:19 +0100

    integrated logging in and out into the front-end, and the page automagically updates based on status. Seems fragile

--- frontend/src/pages/dashboard.rs
+++ frontend/src/pages/dashboard.rs
@@ -4,26 +4,34 @@ use leptos::*;
 use models::{JsonMsg, PartialUser};
 use web_sys::{Headers, RequestMode, Response};
 
-use crate::{store::AppSettings, Fetcher};
+use crate::{
+    store::{AppSettings, AuthState},
+    Fetcher,
+};
 
 #[component]
 fn LogoutButton() -> impl IntoView {
+    let auth_state: AuthState = use_context::<AuthState>().expect("AuthState context not found?");
     let app_settings: AppSettings =
         use_context::<AppSettings>().expect("AppSettings context not found");
 
-    let headers: Headers = Headers::new().unwrap();
-    headers
-        .set("Content-Type", "application/json;charset=UTF-8")
-        .unwrap();
-    let fetcher: Fetcher = Fetcher::init()
-        .set_url(app_settings.backend_url.to_string() + "user-logout")
-        .set_method("GET")
-        .set_headers(headers)
-        .set_mode(RequestMode::Cors)
-        .build();
-
     let logout_action = create_action(move |_| {
-        let response: Response = fetcher.fetch(None).await;
+        let headers: Headers = Headers::new().unwrap();
+        headers
+            .set("Content-Type", "application/json;charset=UTF-8")
+            .unwrap();
+        let fetcher: Fetcher = Fetcher::init()
+            .set_url(app_settings.backend_url.to_string() + "user-logout")
+            .set_method("GET")
+            .set_headers(headers)
+            .set_mode(RequestMode::Cors)
+            .build();
+        async move {
+            let response: Response = fetcher.fetch(None).await;
+            if response.status() == 200 {
+                auth_state.set_authenticated(false);
+            }
+        }
     });
 
     view! {
@@ -39,7 +47,7 @@ pub fn Dashboard() -> impl IntoView {
     let user: PartialUser = use_context().expect("PartialUser Context not set");
     view! {
         <>
-            <button>"Logout"</button>
+            <LogoutButton />
             <nav>"Shoule have own nav bar"</nav>
             <h1>"Welcome back "{user.name}</h1>
             <summary>"Heading for details tag"</summary>
