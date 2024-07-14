cdf5c6d2fe18f24aac0e85c6b941267433097e44
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2024-07-14 21:21:43 +0100

    integrating Delete Quiz into frontend

--- frontend/src/components/dashboard/get_quiz.rs
+++ frontend/src/components/dashboard/get_quiz.rs
@@ -1,9 +1,16 @@
 //! frontend/src/components/dashboard/get_quiz.rs
 //! This component will handle quiz making logic and pass
 //! user to the making questions screen.
-use crate::{components::Card, models::mimic_surreal::SurrealQuiz};
 use leptos::*;
 use serde::{Deserialize, Serialize};
+use web_sys::{Headers, RequestMode, Response};
+
+use crate::{
+    components::Card,
+    models::mimic_surreal::{SurrealQuiz, Thing},
+    store::{AppSettings, AuthState},
+    utils::{DashDisplay, Fetcher, JsonMsg, PartialUser},
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct QuizJsonPkg {
@@ -15,6 +22,7 @@ pub struct QuizJsonPkg {
 pub fn QuizShowCase(
     quiz_list: RwSignal<Vec<SurrealQuiz>>,
     quiz_selector: Callback<SurrealQuiz>,
+    pop_quiz: Callback<SurrealQuiz>,
 ) -> impl IntoView {
     //  -- Create Signals --
     //  -- Create References --
@@ -34,6 +42,7 @@ pub fn QuizShowCase(
                     <QuizExhibit
                         surreal_quiz=this
                         quiz_selector=quiz_selector
+                        pop_quiz=pop_quiz
                     />
                 }
             />
@@ -45,19 +54,55 @@ pub fn QuizShowCase(
 pub fn QuizExhibit(
     surreal_quiz: SurrealQuiz,
     quiz_selector: Callback<SurrealQuiz>,
+    pop_quiz: Callback<SurrealQuiz>,
 ) -> impl IntoView {
-    let mut cloned_quiz = surreal_quiz.clone();
+    // -- Create Signals --
+    let quiz_sig: RwSignal<SurrealQuiz> = create_rw_signal(surreal_quiz);
+
+    // -- Use Context --
+    let app_settings: AppSettings =
+        use_context::<AppSettings>().expect("AppSettings context not found");
+
+    // -- Create Closures
     let take_quiz_closure = move |_| {
-        quiz_selector.call(cloned_quiz.clone());
+        quiz_selector.call(quiz_sig.get());
     };
+
+    // Action To destroy Quiz
+    let destroy_quiz_closure = move |_| {
+        quiz_selector.call(quiz_sig.get());
+    };
+
+    // -- Create Actions --
+    let destroy_quiz_action = create_action(move |quiz_id: &String| {
+        let headers: Headers = Headers::new().unwrap();
+        headers
+            .set("Content-Type", "application/json;charset=UTF-8")
+            .unwrap();
+        let fetcher: Fetcher = Fetcher::init()
+            .set_url(app_settings.backend_url.to_string() + "quiz-nexus")
+            .add_query_param("quiz", &quiz_id)
+            .set_method("DELETE")
+            .set_headers(headers)
+            .set_mode(RequestMode::Cors)
+            .build();
+        async move {
+            let response: Response = fetcher.fetch(None).await;
+            if response.status() == 200 {
+                let del_quiz: SurrealQuiz = Fetcher::response_to_struct(&response).await;
+                pop_quiz.call(del_quiz);
+            }
+        }
+    });
+
     view! {
         <Card on_click=None>
-            <p>"Name: "{surreal_quiz.name}</p>
-            <p>{surreal_quiz.description}</p>
+            <p>"Name: "{move || quiz_sig.get().name}</p>
+            <p>{move || quiz_sig.get().description}</p>
             <button data-note="unimplemented" on:click=take_quiz_closure>"Take Quiz"</button>
             <button data-note="unimplemented">"Edit"</button>
             <button data-note="unimplemented">"Calibrate"</button>
-            <button data-note="unimplemented">"Delete Quiz"</button>
+            <button data-note="unimplemented" on:click=destroy_quiz_closure>"Delete Quiz"</button>
         </Card>
     }
 }
